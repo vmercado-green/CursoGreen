ac005ee1fc84653f5098f63755f723d006f04b57
vmercado-green <sistemasdeinformacion@miredips.org>
2023-01-26 14:34:44 -0500

    sad

--- biblioteca/__manifest__.py
+++ biblioteca/__manifest__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 {
-    'name': 'Administración de la Biblioteca',
+    'name': 'Administración de la Biblioteca ',
     'version': '1',
     'summary': 'Módulo que gestionará muchos aspectos de la Biblioteca',
     'description': 'Su biblioteca local quiere usar Odoo para administrar sus libros y clientes. Necesitarán ayuda para procesar a los clientes sacando libros y para organizar libros y alquileres.',
