3a8ea2c9bdae2968ef52eec338b5e99387950303
vmercado-green <odoo@ampd02.odoo.com>
2023-01-26 04:12:44 +0000

    [ADD] Actualizaciones

--- mision_espacial/__manifest__.py
+++ mision_espacial/__manifest__.py
@@ -1,13 +1,15 @@
 # -*- coding: utf-8 -*-
 {
-    'name': 'Administración de la Biblioteca',
+    
+    'name': ' Misión Espacial de Odoo',
     'version': '1',
-    'description': 'Módulo que gestionará muchos aspectos de la Biblioteca',
-    'summary': 'Su biblioteca local quiere usar Odoo para administrar sus libros y clientes. Necesitarán ayuda para procesar a los clientes sacando libros y para organizar libros y alquileres.',
-    'category': 'Uncategorized',
+    'description': 'Módulo que planifique y ayude a Odoo a llegar a la Luna',
+    'summary': 'Odoo Inc. está intentando visitar la Luna. Necesitan tu ayuda para crear una Aplicación para organizar la logística. Esto incluye la nave espacial y la tripulación',
+    'category': 'Account',
     'author': 'GREEN-VICTOR MERCADO',
     'website':'https://www.greenapplic.com/',
     'license': 'AGPL-3',
+    'depends': ['base'],
     'installable': True,
     'auto_install': False
 }
