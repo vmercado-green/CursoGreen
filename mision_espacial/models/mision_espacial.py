7fe0f4ecd792d2870aa78fb0646414f9578c9ba6
vmercado-green <sistemasdeinformacion@miredips.org>
2023-01-26 16:23:24 -0500

    Incluir campos

--- mision_espacial/models/mision_espacial.py
+++ mision_espacial/models/mision_espacial.py
@@ -10,10 +10,10 @@ class Mision_Espacial (models.Model):
     dimensiones_del_barco = fields.Integer(string='Dimensiones Del Barco', required=True)
 
     combustibles = fields.Selection(string='Combustibles',
-                             selection[('1', 'Criogénicos'),
+                             selection=[('1', 'Criogénicos'),
                              ('2', 'semi criogénicos'),
                              ('3', 'hipergólicos '),
-                             ('4', 'sólidos  ')],
+                             ('4', 'sólidos ')],
                              copy=False, required=True)
     Tipo_De_Barco = fields.Selection(string='Tipo De Barco',
                                     selection=[('1', 'Nave espacial sobrevuelo'),
@@ -24,7 +24,7 @@ class Mision_Espacial (models.Model):
                                     ('6', 'Nave espacial Penetradora'),
                                     ('7', 'Nave espacial Observatorio'),
                                     ('8', 'Nave espacial de comunicaciones')],
-                                    copy=False , required=True)
+                                    copy=False, required=True)
     Numero_De_Pasajeros = fields.Integer(string='Numero De Pasajero', required=True)
 
     active = fields.Boolean(string='Active', default=True)
