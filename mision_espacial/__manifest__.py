9952715eb07d222ba4308115a4962d70e719570a
vmercado-green <odoo@ampd02.odoo.com>
2023-01-26 03:33:07 +0000

    [ADD] MI PRIMER MODULO

---
+++ mision_espacial/__manifest__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Administración de la Biblioteca',
+    'version': '1',
+    'description': 'Módulo que gestionará muchos aspectos de la Biblioteca',
+    'summary': 'Su biblioteca local quiere usar Odoo para administrar sus libros y clientes. Necesitarán ayuda para procesar a los clientes sacando libros y para organizar libros y alquileres.',
+    'category': 'Uncategorized',
+    'author': 'GREEN-VICTOR MERCADO',
+    'website':'https://www.greenapplic.com/',
+    'license': 'AGPL-3',
+    'installable': True,
+    'auto_install': False
+}
