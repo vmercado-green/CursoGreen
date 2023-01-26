42276700bb160cf0679f41cc5725d8da008d20c1
vmercado-green <odoo@ampd02.odoo.com>
2023-01-26 03:33:07 +0000

    [ADD] MI PRIMER MODULO

---
+++ biblioteca/__manifest__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+{
+    'name': ' Misión Espacial de Odoo',
+    'version': '1',
+    'description': 'Módulo que planifique y ayude a Odoo a llegar a la Luna',
+    'summary': 'Odoo Inc. está intentando visitar la Luna. Necesitan tu ayuda para crear una Aplicación para organizar la logística. Esto incluye la nave espacial y la tripulación',
+    'category': 'Uncategorized',
+    'author': 'GREEN-VICTOR MERCADO',
+    'website':'https://www.greenapplic.com/',
+    'license': 'AGPL-3',
+    'installable': True,
+    'auto_install': False
+}
---
+++ primer_modulo/model/__init__.py
@@ -0,0 +1,2 @@
+# -*- coding: utf-8 -*-
+from . import primer_modulo
