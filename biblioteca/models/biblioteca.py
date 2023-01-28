10c3df0ac94f6abcd7fd7e81d56c76cec3bb0dcc
vmercado-green <sistemasdeinformacion@miredips.org>
2023-01-28 16:00:57 -0500

    views

--- biblioteca/models/biblioteca.py
+++ biblioteca/models/biblioteca.py
@@ -11,7 +11,7 @@ class Biblioteca (models.Model):
     autor = fields.Char(string='Autor', required=True)
     editor = fields.Char(string='Editor', required=True)
     editorial = fields.Char(string='Editorial', required=True)
-    año_edicion = fields.Char(string='Año Edicion', required=True)
+    year_edicion = fields.Char(string='Año Edicion', required=True)
     genero = fields.Selection(string='Tipo De Barco',
                                      selection=[('1', 'Aventuras'),
                                      ('2', 'Ciencia ficción'),
